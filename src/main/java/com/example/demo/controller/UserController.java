2527ecbe434c6da769b6b31a16796149ed9a613f
NoeShinohara <163381729+roses-5117@users.noreply.github.com>
2025-08-05 17:25:33 +0900

    ユーザー削除用コントローラの作成・

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.data.entity.User;
@@ -16,48 +17,58 @@ import com.example.demo.form.UserForm;
 
 @Controller
 public class UserController {
+
 	// @Autowiredアノテーションを付けると、Spring Bootが自動でインスタンスをInjectします。
 	@Autowired
 	private UserRepository userRepository;
 
-	// @RequestMapping(path = "/user", method = RequestMethod.GET)の省略版。
-	// HTTPのメソッドGETのみ受け付けます。
 	@GetMapping("/users")
 	// 引数にorg.springframework.ui.Modelを追加
 	public String getUsers(Model model) {
-		// ユーザーリスト取得処理を追加
 		List<User> users = userRepository.findAll();
-		// 取得したリストをテンプレートに渡す
+		// Modelにusersを追加
 		model.addAttribute("users", users);
 		return "users";
 	}
 
 	// getNewUserメソッドを追加
 	@GetMapping("/newuser")
+	// 引数にModelを追加
 	public String getNewUser(Model model) {
 		// Modelに空のUserFormを追加
 		UserForm userForm = new UserForm();
 		model.addAttribute("userForm", userForm);
-		// テンプレートは src/main/resources/templates/newuser.html とします。
 		return "newuser";
 	}
 
 	// マッピング設定
 	@PostMapping("/newuser")
-	// 引数にBindingResultを追加
+	// 引数にUserFormを追加
 	public String registerUser(@Validated UserForm userForm, BindingResult bindingResult) {
 		// バリデーションの結果、エラーがあるかどうかチェック
 		if (bindingResult.hasErrors()) {
 			// エラーがある場合はユーザー登録画面を返す
 			return "newuser";
 		}
+
+		// UserFormの値をUserクラス（Entity）にセットする
 		User user = new User();
 		user.setName(userForm.getName());
 		user.setEmail(userForm.getEmail());
 
 		// データベースに保存
 		userRepository.save(user);
-		// ユーザ一覧画面へリダイレクト
+
+		return "redirect:/users";
+	}
+
+	// deleteUserメソッドを追加
+	// リクエストマッピング設定を追加
+	@PostMapping("/users/delete/{id}")
+	// 処理の中でidを使えるように、引数にidを追加
+	public String deleteUser(@PathVariable Long id) {
+		// 指定したIDのユーザーを削除
+		userRepository.deleteById(id);
 		return "redirect:/users";
 	}
 }
